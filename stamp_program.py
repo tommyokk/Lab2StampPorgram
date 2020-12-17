0558b8b4886b4dff30222fa567087251c5c822ca
tommyokk <70242731+tommyokk@users.noreply.github.com>
2020-12-17 06:27:14 -0800

    Update stamp_program.py

--- stamp_program.py
+++ stamp_program.py
@@ -6,17 +6,3 @@ round answer to next number
 output result to user
 end
 """
-
-import math
-
-def calculate(sheet):
-    answer = sheet / 5
-    rounded = math.ceil(answer)
-    print("sheet is: ", sheet)
-    print("the answer is:", answer)
-    print("rounded is:", rounded)
-    print("========================================")
-    return rounded
-
-    output = calculate(16)
-    print ("the answer of stamps needed is:", output)
